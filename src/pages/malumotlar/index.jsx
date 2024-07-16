47499c47265b73f0b76be570228938ec63376b3b
Humoyun <humoyun@MacBook-Air.local>
2024-07-16 15:57:06 +0500

    Information about the company

--- src/pages/malumotlar/index.jsx
+++ src/pages/malumotlar/index.jsx
@@ -1,12 +1,78 @@
 import "./style.scss";
+import React from 'react';
+import { Layout, Typography, List, Divider } from 'antd';
+import { Container } from "@containers";
+
+const { Content } = Layout;
+const { Title, Paragraph, Text } = Typography;
 
 function Index() {
-  return <>
-    <div className="container mx-auto px-2 5">
-      <h1 className="text-center py-2 text-[20px] font-semibold">Malumtlar</h1>
-      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Numquam, amet quae aut ex quis praesentium vel dicta doloremque officia fugiat, perferendis eos rem iure recusandae ad consectetur qui repellendus repellat atque aliquid animi error minus cum iste? Consequuntur corporis dolorem magnam facere dolores laborum corrupti distinctio, vitae ullam modi dolor in optio placeat nam ex, blanditiis quos dignissimos fugit excepturi sapiente aperiam harum quod quaerat error? Quo nam sed neque aperiam itaque sequi veniam ipsa nobis, pariatur et eligendi quia sunt repellat molestiae accusamus deleniti enim, dolor officia similique. Porro.</p>
-    </div>
-  </>
+  const partners = [
+    "RFbeam Microwave GmbH",
+    "MaxBotix Inc",
+    "Xylem (Hong Kong) Limited",
+    "NEW DAXIN TECHNOLOGY CO. LIMITED",
+    "DianYingPu (HK) Technology Co Ltd"
+  ];
+
+  const clients = [
+    "O’zbekiston Respublikasi suv xo’jaligi vazirligi",
+    "O’zbekiston Respublikasi suv xo’jaligi vazirligi huzuridagi Amu-Buxoro ITHB",
+    "Qurilish Ta’mirlash MChJ",
+    "Amu Buxoro ITHB",
+    "Janubiy Mirzacho’l magistral kanalidan foydalanish boshqarmasi",
+    "Katta Farg’ona magistral kanali boshqarmasi",
+    "TIIIMSX"
+  ];
+
+  return (
+    <Layout>
+      <Content style={{ padding: '50px' }}>
+        <Container>
+          <div>
+            <Title level={2}>Kompanya haiqida</Title>
+          </div>
+
+          <Divider />
+
+          <div>
+            <Title level={3}>Kompaniya haqida</Title>
+            <Paragraph>
+              <Text strong>«Smart Solutions System»</Text> kompaniyasi 2019-yilda tashkil topgan. Bugungi kunda kompaniya O’zbekistonda aqlli texnologiyalarni joriy qilishda ko’plab yutuqlarga erishib kelmoqda. Smart Water, Smart Well aqlli qurilmalari aynan «Smart Solutions System» kompaniyasi tomonidan ishlab chiqilgan. Bundan tashqari suv inshootlarini, nasos stansiyalarini, gidro uzellarni va suv omborlarini avtomatlashtirish va monitoring qilish ham kompaniyaning asosiy yo’nalishlaridan biri. Yurtimizda energetika soxasida olib borilayotgan islohotlarda jumladan quyosh panellari va mini GESlar soxasida ham ishlar olib borilmoqda. Ayni vaqtda kompaniyada doimiy asosda 30 dan ortiq xodim ishlamoqda.
+            </Paragraph>
+          </div>
+
+          <Divider />
+
+          <div>
+            <Title level={3}>Hamkorlar</Title>
+            <List
+              dataSource={partners}
+              renderItem={item => (
+                <List.Item>
+                  <Text>{item}</Text>
+                </List.Item>
+              )}
+            />
+          </div>
+
+          <Divider />
+
+          <div>
+            <Title level={3}>Bizning mijozlarimiz</Title>
+            <List
+              dataSource={clients}
+              renderItem={item => (
+                <List.Item>
+                  <Text>{item}</Text>
+                </List.Item>
+              )}
+            />
+          </div>
+        </Container>
+      </Content>
+    </Layout>
+  );
 }
 
-export default Index
+export default Index;
