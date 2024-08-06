777561c028bfdf5b743c6b91b4e0abfdf32646b9
Humoyun <humoyun@MacBook-Air.local>
2024-08-06 18:07:15 +0500

    team

--- src/pages/malumotlar/index.jsx
+++ src/pages/malumotlar/index.jsx
@@ -10,8 +10,6 @@ import Fozilov from "../../assets/images/fozilov.jpg";
 import Husan from "../../assets/images/husan.jpg";
 import Nodirbek from "../../assets/images/nodirbek.jpg";
 
-import Sirojbek from "../../assets/images/husan.jpg";
-
 const teamMembers = [
   {
     name: "Fozilov Ilyosjon",
@@ -82,7 +80,7 @@ const teamMembers = [
   {
     name: "Choriev Nodirbek    ",
     position: "Yangi ishlanmalar, ilmiy tadqiqotlar va sinov ishlarini amalga oshirish bo'limi boshlig'i",
-    image: Nodirbek,
+
     social: {
       telegram: "https://t.me/username",
       instagram: "https://instagram.com/username",
@@ -112,7 +110,7 @@ function Index() {
     
    
     <div className="company">
-      <h1>Kompaniya haqida</h1>
+    
         <div className="company-wrap">
           <div className="company-text">
             <h2>Kompaniya haqida</h2>
@@ -133,9 +131,7 @@ function Index() {
     <div className="partners">
       <h3>Hamkorlar</h3>
       <p>
-        Kompaniya RFbeam Microwave GmbH, MaxBotix Inc, Xylem (Hong Kong) Limited,
-        NEW DAXIN TECHNOLOGY CO. LIMITED, DianYingPu (HK) Technology Co Ltd kabi
-        kompaniyalarning hamkori hisoblanadi.
+      Smart Solutions System» kompaniyasi 2019-yilda tashkil topgan. Bugungi kunda kompaniya O’zbekistonda aqlli texnologiyalarni joriy qilishda ko’plab yutuqlarga erishib kelmoqda. Smart Water, Smart Well aqlli qurilmalari aynan«Smart Solutions System» kompaniyasi tomonidan ishlab chiqilgan. Bundan tashqari suv inshootlarini, nasos stansiyalarini, gidro uzellarni va suv omborlarini avtomatlashtirish va monitoring qilish ham kompaniyaning asosiy yo’nalishlaridan biri. Yurtimizda energetika soxasida olib borilayotgan islohotlarda jumladan quyosh panellari va mini GESlar soxasida ham ishlar olib borilmoqda. Ayni vaqtda kompaniyada doimiy asosda 60 dan ortiq xodim ishlamoqda.
       </p>
     </div>
 
@@ -154,24 +150,31 @@ function Index() {
         </div>
 
           <div className="team">
-      <h1>Jamoa a'zolari</h1>
-      <div className="team-members">
-        {teamMembers.map((member, index) => (
-          <div key={index} className="team-card">
-            <img src={member.image} alt={member.name} />
-            <h2>{member.name}</h2>
-            <h3>{member.position}</h3>
-            <div className="social-icons">
-              <ul>
-                <li><a href={member.social.telegram}><i className="bi bi-telegram"></i></a></li>
-                <li><a href={member.social.instagram}><i className="bi bi-instagram"></i></a></li>
-                <li><a href={member.social.email}><i className="bi bi-envelope"></i></a></li>
-                <li><a href={member.social.whatsapp}><i className="bi bi-whatsapp"></i></a></li>
-              </ul>
-            </div>
-          </div>
-        ))}
-      </div>
+             <h1>Jamoa a'zolari</h1>
+              <div className="team-members">
+                {teamMembers.map((member, index) => (
+                  <div key={index} className="team-card">
+
+                    <div className="team-card-photo">
+                    <img src={member.image} alt={member.name} />
+                    </div>
+                    
+                    <div>
+                    <h2>{member.name}</h2>
+                    <h3>{member.position}</h3>
+                    <div className="social-icons">
+                      <ul>
+                        <li><a href={member.social.telegram}><i className="bi bi-telegram"></i></a></li>
+                        <li><a href={member.social.instagram}><i className="bi bi-instagram"></i></a></li>
+                        <li><a href={member.social.email}><i className="bi bi-envelope"></i></a></li>
+                        <li><a href={member.social.whatsapp}><i className="bi bi-whatsapp"></i></a></li>
+                      </ul>
+                    </div>
+                    </div>
+                    
+                  </div>
+                ))}
+              </div>
          </div>
      
     </div>
